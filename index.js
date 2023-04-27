c6f0c5158a248068f532a85732b43fbdf72014f9
mac <mac@MacBook-Pro-mac.local>
2023-04-27 20:31:44 +0300

    4

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ dotenv.config()
 import express from 'express'
 import cors from 'cors'
 import connectDB from './db.js'
+import Url from './Url.js'
+import dns from 'dns'
 
-
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 const app = express();
 
 // Basic Configuration
@@ -25,11 +29,37 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl',(req,res)=>{
-  
-  console.log(req.body)
-  res.json(req.body)
+app.post('/api/shorturl', async (req,res)=>{
+  const {url} = req.body
+  dns.lookup(url,(err, adr, fam)=>{
+    if(err){
+      res.json({ error: 'invalid url' })
+      return
+    }
+  })
+  const base = process.env.BASE
+  let urlNew = await Url.findOne({originUrl: url})
+  if(urlNew){
+    res.json({original_url: urlNew.originUrl, short_url: urlNew.urlId})
+    return
+  }
+  const urlId = (getRandomInt(1, 1000)).toString();
+  const shortUrl = `${url}/${urlId}`
+  urlNew = new Url({
+    urlId: urlId,
+    originUrl: url,
+    shortUrl: shortUrl
+  })
+  await urlNew.save()
+  res.json({original_url: urlNew.originUrl, short_url: urlNew.urlId})
+})
 
+app.get('/api/shorturl/:urlId',async (req,res)=>{
+  const {urlId} = req.params
+  const result = await Url.findOne({urlId})
+  if(result){
+    res.redirect(result.originUrl)
+  }else res.status(404).json('Not found')
 })
 
 app.listen(port, function() {
